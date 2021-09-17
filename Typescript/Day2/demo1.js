a5565dae1553f809931d3811d4ba09877dab26cf
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-09-17 12:45:25 +0530

    Day2

---
+++ Typescript/Day2/demo1.js
@@ -0,0 +1,18 @@
+//core java 
+// System.out.println("Hello World");
+// console.log("Hello World");
+// core java int a;
+//rules of identifier
+// It should start with letter after that we can use number
+// _ $
+//no any special keyword use
+//no any whitespace allowed
+var a1;
+var a2, a3;
+//datatype
+// number(int,long,double,float,short), string (string,char=>"sumit"/'s'), 
+//boolean (true/false), any(number,string,boolean), void (netural)
+// mises. null & undefined
+var b1 = 10; //Forward declaration & defination
+console.log('Value of b1 is ' + b1);
+console.log("======== Value of b1 is " + b1);
