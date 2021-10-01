c1e06032e78ae49eeeeac7e7338a61aed3213936
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-10-01 12:49:02 +0530

    Day3

---
+++ Angular/Day3/stringinterpolation/stringinterpolation.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stringinterpolation',
+  templateUrl: './stringinterpolation.component.html',
+  styleUrls: ['./stringinterpolation.component.css']
+})
+export class StringinterpolationComponent implements OnInit {
+
+    name:string='Sumit raokhande';
+    status:boolean=true;
+    imgUrl:string='../../assets/bg.jpeg';
+    
+    
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
