d83ab930593aa25261c552fe38762ae0797a6ea6
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-10-25 16:26:27 +0530

    Day16

---
+++ Angular/Day16/post/post.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  posts:any[]=[];
+  id:number;
+  title:string='';
+  body:string='';
+  isHidden:boolean=true;
+
+  constructor(private service:HttpService) { }
+
+  ngOnInit() {
+    this.GetPost();
+  }
+
+  GetPost(){
+    this.service.getpost()
+    .subscribe((response)=>{
+      console.log(response)
+
+      this.posts=(<any>response);
+
+    })
+  }
+
+  OnSend(inputTitle,inputBody){
+
+    let obj={
+      title:inputTitle,
+      body:inputBody
+    }
+
+    this.service.PostData(obj)
+    .subscribe((response)=>{
+      console.log(response)
+    })
+  }
+
+  onEdit(item){
+      this.id=item.id;
+      this.title=item.title;
+      this.body=item.body;
+      this.isHidden=false;
+  }
+
+  onUpdate(){
+    let obj={
+      id:this.id,
+      title:this.title,
+      body:this.body
+    }
+
+    this.service.UpdateData(obj)
+    .subscribe((response)=>{
+      console.log(response);
+    })
+  }
+
+}
