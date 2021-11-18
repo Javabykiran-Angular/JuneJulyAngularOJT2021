45235a44bd84052aa99144dab3d508171270a39f
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-11-18 15:27:18 +0530

    Day29

---
+++ Angular/Day29/http.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private BaseUrl:string="http://localhost:8080/api/";
+  
+  constructor(private http:HttpClient) { }
+
+  LoginCheck(loginobj){
+    return  (this.http.post(`${this.BaseUrl}logincheck`,loginobj));
+  }
+
+  getAllEmployee(){
+    return (this.http.get(`${this.BaseUrl}getallemployee`));
+  }
+
+  getAllCountry(){
+    return (this.http.get(`${this.BaseUrl}getallcountry`));
+  }
+
+  AddEmployee(obj){
+    return  (this.http.post(`${this.BaseUrl}addemployee`,obj,
+      {responseType:'text'}));
+  }
+
+
+}
