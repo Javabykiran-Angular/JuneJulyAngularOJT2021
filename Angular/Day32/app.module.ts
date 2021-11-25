c480d341defc7e6931c35b470ab0095c8a011ce4
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-11-25 15:24:32 +0530

    Day32

---
+++ Angular/Day32/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ParticlesModule } from 'angular-particle'; 
+import { NgxCaptchaModule } from 'ngx-captcha';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';  
+import { NgxPaginationModule } from 'ngx-pagination';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    AddemployeeComponent,
+    NotfoundComponent,
+    EmployeeDetailsComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ParticlesModule,      //It is used for Angular- particle
+    NgxCaptchaModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Day32/update-employee/update-employee.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Employee } from '../models/employee';
+
+@Component({
+  selector: 'app-update-employee',
+  templateUrl: './update-employee.component.html',
+  styleUrls: ['./update-employee.component.css']
+})
+export class UpdateEmployeeComponent implements OnInit {
+
+
+  @Input() parentdata=<Employee>{};
+  allCountry=[];
+  issubmitDissabled:boolean=true;
+  backendResponse:string='';
+
+  constructor(private service:HttpService) { }
+
+
+  ngOnInit() {
+    this.GetAllCountry();
+  }
+
+  GetAllCountry(){
+        this.service.getAllCountry()
+        .subscribe((response)=>{
+          this.allCountry=(<any>response)
+        })
+  }
+
+  onupdate(){
+
+    this.parentdata.updatedby=sessionStorage.getItem("username");
+    this.parentdata.updateddtm=Date.now();
+    
+
+      this.service.UpdateEmployee(this.parentdata)
+      .subscribe((response)=>{
+        // console.log(response);
+        this.backendResponse=response;
+        this.issubmitDissabled=false;
+      })
+  }
+
+}
